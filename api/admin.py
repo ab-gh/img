f32e786c27e90d48cfb8e8045396f259f34210ff
aejb <git@ash.pm>
2020-12-14 17:22:12 +0000

    populates User and Image on admin dash

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import User, Image
+
 # Register your models here.
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "content", "user", "image")
+
+
+admin.site.register(User)
+admin.site.register(Image, ImageAdmin)
