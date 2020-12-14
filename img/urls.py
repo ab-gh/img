5a6709918b1aefc324178d2523295879815941db
aejb <git@ash.pm>
2020-12-14 22:36:07 +0000

    add openapi

--- img/urls.py
+++ img/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include("api.urls")),
+    path('openapi', get_schema_view(
+        title="Your Project",
+        description="API for all things …",
+        version="1.0.0"
+    ), name='openapi-schema'),
+
+    
 ]
