a0de878460d7070564dde0d227d8b7e9011866d9
aejb <git@ash.pm>
2020-12-15 17:54:13 +0000

    enables swagger UI at /

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
+from django.views.generic import TemplateView
 from api import views
 
 urlpatterns = [
     path('images/', views.ImageList.as_view()),
-    path('images/<int:pk>', views.ImageDetail.as_view())
+    path('images/<int:pk>', views.ImageDetail.as_view()),
+    path('', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url':'openapi-schema'}
+    ), name='swagger-ui'),
 ]
